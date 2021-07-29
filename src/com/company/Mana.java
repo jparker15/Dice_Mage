0ddbde922e4adb0fc9ebbd578b689e5f79301c6a
Jeremy <jeremyeparker@yahoo.com>
2021-07-28 23:04:02 -0400

    mana pool of dice

--- src/com/company/Mana.java
+++ src/com/company/Mana.java
@@ -1,2 +1,39 @@
-package com.company;public class Mana {
+package com.company;
+
+
+public class Mana {
+
+        public ArrayList<Die> mana = new ArrayList<>();
+
+        public Mana (){
+
+        }
+
+        public void roll(){
+            // for each die in dice
+            for (var die: dice) {
+
+            }
+        }
+        // single roll die
+        public void roll (int choice){
+
+        }
+
+        public void roll ( List<Integer> choices){
+            for (int choice: choices) {
+
+//            roll(rand,choice);
+            }
+        }
+
+        @Override
+        public String toString(){
+            String output = "";
+            for (Die die:dice) {
+                output += die.value + "";
+            }
+            return output.trim();
+        }
+
 }
