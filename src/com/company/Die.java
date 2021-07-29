0de892a637ca2d48db3bad43de5d7f886fc616a6
Jeremy <jeremyeparker@yahoo.com>
2021-07-28 23:03:50 -0400

    die with mutable sides

---
+++ src/com/company/Die.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Die {
+    public int value;
+    public int numOfSides;
+
+    public Die (){
+        numOfSides = 6;
+
+    }
+
+    public Die(int numOfSides){
+        this.numOfSides = numOfSides;
+    }
+
+    public void roll () {
+        value = (int) ((Math.random() * numOfSides) + 1); //1 - 6
+    }
+
+}
