6cbb23ef4cd2362294ccf26af38f441f9256b8b8
Jeremy <jeremyeparker@yahoo.com>
2021-07-29 23:41:18 -0400

    mana poool

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Player p1 = new Player("Jar");
+
+        p1.mana.manaPool = 1;
+
+        System.out.println(p1.mana.manaPool);
     }
 }
