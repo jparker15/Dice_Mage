ce4fa49826ad2ad14dc8d85769c88871e55a9713
Jeremy <jeremyeparker@yahoo.com>
2021-08-03 23:35:46 -0400

    player 1 turn

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,16 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Player p1 = new Player("Jar");
+        Player p1 = new Player();
 
-        p1.mana.manaPool = 1;
+        p1.aether.roll();
 
-        System.out.println(p1.mana.manaPool);
+        System.out.println(p1.aether.display());
 
-        if(true){
-            p1.mana.manaPool += 1;
-        }else{
-            p1.mana.manaPool = 0;
-        }
+        int managen =  p1.aether.generateMana();
+
+        p1.mana += managen;
+
+        System.out.println(p1.mana
+        );
     }
 }
