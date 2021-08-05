1bcad7f154d0883b393cc9aca9487ecbce6a7bab
Jeremy <jeremyeparker@yahoo.com>
2021-08-05 18:47:00 -0400

    add monster to den

--- src/com/company/Den.java
+++ src/com/company/Den.java
@@ -1,2 +1,12 @@
-package com.company;public class Den {
+package com.company;
+
+import java.util.ArrayList;
+
+public class Den {
+
+    public ArrayList<Die> den = new ArrayList();
+
+    public void addMonster(){
+        den.add(new Die());
+    }
 }
