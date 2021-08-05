3ef08d66937d0abf1e306472be5ce015c3438390
Jeremy <jeremyeparker@yahoo.com>
2021-08-05 18:47:28 -0400

    add Den

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -3,11 +3,18 @@ package com.company;
 public class Player {
     public String name;
     public Aether aether = new Aether();
+    public Den den = new Den();
     public int health = 5;
     public int mana = 0;
 
 
     public Player(){
         this.name = name;
+    }
+
+    public void displayStats(){
+        System.out.println("\nMage " + name);
+        System.out.println("Health: " + health + " | Aether: " + aether.totalPool.size() + " | Mana: " + mana);
+
     }
 }
