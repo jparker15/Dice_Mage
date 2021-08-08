b84b4e5e425819683882fc64d9715964532d306f
jeremy <jeremyeparker@yahoo.com>
2021-08-07 23:47:19 -0400

    monster field

--- src/com/company/Den.java
+++ src/com/company/Den.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class Den {
 
     public ArrayList<Die> den = new ArrayList();
+    public Die mon = new Die();
 
     public void addMonster(){
         den.add(new Die());
