7f040460cf3a51901101fa92ff4fe10202b7a949
jeremy <jeremyeparker@yahoo.com>
2021-08-09 17:25:12 -0400

    transfer gameplay to self class

--- src/com/company/DiceMage.java
+++ src/com/company/DiceMage.java
@@ -1,6 +1,51 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class DiceMage {
 
+    public ArrayList<Player> players = new ArrayList<>();
+    public int currentPlayer = 0;
 
+    public DiceMage(){
+        Player p1 = new Player();
+        Player p2 = new Player();
+
+        players.add(p1);
+        players.add(p2);
+        Scanner scanner = new Scanner(System.in);
+
+        for(Player p : players){
+            System.out.println("Name of Player?:");
+            String playerName = scanner.next();
+            p.name = playerName;
+        }
+
+        p1.aether.roll();
+        System.out.println(p1.aether.display());
+        int managen =  p1.aether.generateMana();
+        p1.mana += managen;
+        System.out.println(p1.name + " gained " + managen );
+        p1.displayStats();
+        if(p1.mana > p1.aether.totalPool.size() - 2){
+            System.out.println("add to the Aether?");
+            System.out.println(p1.aether.totalPool.size());
+            String answer = scanner.next();
+            if(answer.equals("y")){
+                p1.mana -= (p1.aether.totalPool.size() - 2);
+                p1.aether.addAether();
+                p1.displayStats();
+            }
+        }
+
+        if(p1.mana > 6){
+            System.out.println("summon monster?");
+            String answer = scanner.next();
+            if(answer.equals("y")){
+                p1.den.addMonster();
+                p1.mana -= p1.den.den.size() * 3;
+            }
+        }
+    }
 }
