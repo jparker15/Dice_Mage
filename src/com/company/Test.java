e10afe1d2cb8f140f29ba64129574703db757876
Jeremy <jeremyeparker@yahoo.com>
2021-08-10 20:26:10 -0400

    test with hard code fields

--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -1,2 +1,19 @@
-package com.company;public class Test {
+package com.company;
+
+public class Test {
+    public static void main(String[] args) {
+        DiceMage dice = new DiceMage();
+
+        Player p1 = new Player("Wyt");
+        Player p2 = new Player("Blk");
+
+        dice.players.add(p1);
+        dice.players.add(p2);
+
+
+        while(true){
+            dice.start();
+
+        }
+    }
 }
