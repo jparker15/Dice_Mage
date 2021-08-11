7c72097cb161736b14c08e203b69c2939336498e
Jeremy <jeremyeparker@yahoo.com>
2021-08-10 20:25:48 -0400

    ask players names

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,9 +10,11 @@ public class Main {
 	// write your code here
 
     DiceMage dice = new DiceMage();
+        Player p1 = new Player();
+        Player p2 = new Player();
 
-
-
+        dice.players.add(p1);
+        dice.players.add(p2);
 
     }
 
