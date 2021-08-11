c6b4e57e99229d4f425145ef639f1b44ed452496
Jeremy <jeremyeparker@yahoo.com>
2021-08-10 20:25:29 -0400

    add turn method

--- src/com/company/DiceMage.java
+++ src/com/company/DiceMage.java
@@ -6,49 +6,59 @@ import java.util.Scanner;
 public class DiceMage {
 
     public ArrayList<Player> players = new ArrayList<>();
-    public int currentPlayer = 0;
-
+    Scanner scanner = new Scanner(System.in);
     public DiceMage(){
-        Player p1 = new Player();
-        Player p2 = new Player();
 
-        players.add(p1);
-        players.add(p2);
-        Scanner scanner = new Scanner(System.in);
+    }
 
-        for(Player p : players){
-            System.out.println("Name of Player?:");
-            String playerName = scanner.next();
-            p.name = playerName;
+    public void start(){
+//        for(Player p : players){
+//            System.out.println("Name of Player?:");
+//            String playerName = scanner.next();
+//            p.name = playerName;
+//        }
+        for(Player p: players){
+            playerTurn(p);
         }
-        while(true){
-            p1.aether.roll();
-            System.out.println(p1.aether.display());
-            int managen = p1.aether.generateMana();
-            p1.mana += managen;
-            System.out.println(p1.name + " gained " + managen);
-            p1.displayStats();
-            if (p1.mana > p1.aether.totalPool.size() - 2) {
-                System.out.println("add to the Aether?");
-                System.out.println(p1.aether.totalPool.size());
+    }
+
+    public void playerTurn(Player player){
+            //Random Mana Bonus
+            player.aether.roll();
+            System.out.println(player.aether.display());
+            //total Mana gained
+            int managen = player.aether.generateMana();
+            player.mana += managen;
+
+            System.out.println(player.name + " gained " + managen);
+
+            System.out.println(player.name + "'s mana is " + player.mana);
+
+            if (player.mana > player.aether.totalPool.size() - 2) {
+                System.out.println("add to the Aether?"  );
+                System.out.println(player.aether.totalPool.size());
                 String answer = scanner.next();
                 if (answer.equals("y")) {
-                    p1.mana -= (p1.aether.totalPool.size() - 2);
-                    p1.aether.addAether();
-                    p1.displayStats();
+                    player.mana -= (player.aether.totalPool.size() - 2);
+                    player.aether.addAether();
+                    player.displayStats();
                 }
             }
 
-            if (p1.mana >= 3) {
+            if (player.mana >= 3) {
                 System.out.println("summon monster?");
                 String answer = scanner.next();
                 if (answer.equals("y")) {
-                    p1.den.addMonster();
-                    p1.mana -= p1.den.den.size() * 3;
+                    player.den.addMonster();
+                    player.mana -= player.den.den.size() * 3;
                 }
             }
 
-            p1.displayStats();
-        }
+            else{
+
+            }
+
+            player.displayStats();
+
     }
 }
