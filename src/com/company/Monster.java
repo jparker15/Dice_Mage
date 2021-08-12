6ea6b8180154b1b7366fef17c4e04dbab0a96dbc
Jeremy <jeremyeparker@yahoo.com>
2021-08-12 19:31:38 -0400

    seperate from Die class

--- src/com/company/Monster.java
+++ src/com/company/Monster.java
@@ -1,2 +1,16 @@
-package com.company;public class Monster {
+package com.company;
+
+public class Monster {
+
+    public int powerLvl;
+    public int attackLvl;
+
+    public Monster(int powerLvl){
+        this.powerLvl = powerLvl;
+
+    }
+
+   public void attack (){
+       attackLvl = (int) ((Math.random() * powerLvl)); //roll an attack in a range from 0 to power level
+   }
 }
