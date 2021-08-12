3018bbb5a44da4e90be12ab856e8144f9885334e
Jeremy <jeremyeparker@yahoo.com>
2021-08-12 19:30:48 -0400

    add attack method

--- src/com/company/Den.java
+++ src/com/company/Den.java
@@ -4,18 +4,29 @@ import java.util.ArrayList;
 
 public class Den {
 
-    public ArrayList<Die> den = new ArrayList();
+    public ArrayList<Monster> den = new ArrayList();
+    public int totalAtk = 0;
 
 
     public void addMonster(){
-        den.add(new Die(den.size() + 1 * 2));
+        den.add(new Monster(den.size() + 1 * 2));
+    }
+
+    public int attack(){
+
+        for(Monster m: den){
+            m.attack();
+            totalAtk += m.attackLvl;
+        }
+
+        return totalAtk;
     }
 
     public void display(){
         System.out.println("Den:");
 
-        for(Die monster: den){
-            System.out.println("d" + monster.faceUpValue);
+        for(Monster monster: den){
+            System.out.println("d" + monster.powerLvl);
         }
     }
 }
